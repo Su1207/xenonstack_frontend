d19f42c08f6067cbf616a6e7afbc84d24a036a4c
suraj maheshwari <surajmaheshwari159@gmail.com>
2024-09-11 21:31:43 +0530

    next commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -46,33 +46,33 @@ export default function Login() {
     }
   };
 
-  if (loading) {
-    return (
-      <div className="h-[90vh] w-full flex justify-center items-center">
-        <svg
-          className="animate-spin h-5 w-5 mr-3"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <circle
-            className="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            strokeWidth="4"
-          ></circle>
-          <path
-            className="opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C6.477 0 2 4.477 2 10h2zm2 5.291A7.962 7.962 0 014 12H2c0 3.314 2.686 6 6 6v-2.709z"
-          ></path>
-        </svg>
-        <div className="font-semibold">loading</div>
-      </div>
-    );
-  }
+  //   if (loading) {
+  //     return (
+  //       <div className="h-[90vh] w-full flex justify-center items-center">
+  //         <svg
+  //           className="animate-spin h-5 w-5 mr-3"
+  //           xmlns="http://www.w3.org/2000/svg"
+  //           fill="none"
+  //           viewBox="0 0 24 24"
+  //         >
+  //           <circle
+  //             className="opacity-25"
+  //             cx="12"
+  //             cy="12"
+  //             r="10"
+  //             stroke="currentColor"
+  //             strokeWidth="4"
+  //           ></circle>
+  //           <path
+  //             className="opacity-75"
+  //             fill="currentColor"
+  //             d="M4 12a8 8 0 018-8V0C6.477 0 2 4.477 2 10h2zm2 5.291A7.962 7.962 0 014 12H2c0 3.314 2.686 6 6 6v-2.709z"
+  //           ></path>
+  //         </svg>
+  //         <div className="font-semibold">loading</div>
+  //       </div>
+  //     );
+  //   }
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
